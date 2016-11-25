2a869754bdfd9a21029582458f4a74413b921b4d
Thady Condon <thadycondon@hotmail.com>
2016-11-25 16:52:02 +0000

    Running for chicken and pork

--- lib/nebula_recipe_bot.rb
+++ lib/nebula_recipe_bot.rb
@@ -5,19 +5,30 @@ require 'dotenv'
 Dotenv.load
 
 token = ENV["TELEGRAM_TOKEN"]
-app_id = ENV["APP_ID"]
-app_key = ENV["APP_KEY"]
-url = "https://api.edamam.com/search?q=chicken&app_id=#{app_id}&app_key=#{app_key}"
+@app_id = ENV["APP_ID"]
+@app_key = ENV["APP_KEY"]
+# url = "https://api.edamam.com/search?q=chicken&app_id=#{app_id}&app_key=#{app_key}"
 
-recipe = open(url)
-@label = JSON.parse(recipe.read)
-@labels = []
+# recipe = open(url)
+# @label = JSON.parse(recipe.read)
+# @labels=[]
+
+def get_recipe(request)
+  recipe = open("https://api.edamam.com/search?q=#{request}&app_id=#{@app_id}&app_key=#{@app_key}")
+  @label = JSON.parse(recipe.read)
+end
 
 def get_ingredients
+  @labels=[]
+  @photos=[]
+  @recipe=[]
   @label['hits'].each_with_index do |title, index|
     @labels << "#{index+1}. #{title['recipe']['label']}"
+    @photos << "#{title['recipe']['image']}"
   end
 end
+
+# get_recipe('chicken')
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
@@ -29,6 +40,18 @@ Telegram::Bot::Client.run(token) do |bot|
     when '/recipes'
       get_ingredients
       bot.api.send_message(chat_id: message.chat.id, text: "#{@labels.join(", \n")}")
+    when 'pork'
+      get_recipe("#{message.text}")
+      get_ingredients
+      bot.api.send_message(chat_id: message.chat.id, text: "#{@labels.join(", \n")}")
+    when 'chicken'
+      get_recipe("#{message.text}")
+      get_ingredients
+      bot.api.send_message(chat_id: message.chat.id, text: "#{@labels.join(", \n")}")
+    when '1 image'
+      bot.api.send_photo(chat_id: message.chat.id, photo: "#{@photos[0]}")
+    when '2 image'
+      bot.api.send_photo(chat_id: message.chat.id, photo: "#{@photos[1]}")
     end
   end
 end
