ccfedb4852243bf7702c926dde5cbb3385d39225
richo225 <richo225@gmail.com>
2016-11-25 15:45:25 +0000

    Adds /recipes method that outputs all chicken recipes

--- lib/nebula_recipe_bot.rb
+++ lib/nebula_recipe_bot.rb
@@ -10,15 +10,14 @@ app_key = ENV["APP_KEY"]
 url = "https://api.edamam.com/search?q=chicken&app_id=#{app_id}&app_key=#{app_key}"
 
 recipe = open(url)
+@label = JSON.parse(recipe.read)
+@labels = []
 
-labels = []
-label = JSON.parse(recipe.read)
-
-label['hits'].each do |title|
-  labels << title['recipe']['label']
+def get_ingredients
+  @label['hits'].each_with_index do |title, index|
+    @labels << "#{index+1}. #{title['recipe']['label']}"
+  end
 end
-
-p labels
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
@@ -27,6 +26,9 @@ Telegram::Bot::Client.run(token) do |bot|
       bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
     when '/stop'
       bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+    when '/recipes'
+      get_ingredients
+      bot.api.send_message(chat_id: message.chat.id, text: "#{@labels.join(", \n")}")
     end
   end
 end
