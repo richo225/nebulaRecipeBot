d156bfe72330884584bb3b726a4590cafd448008
Thady Condon <thadycondon@hotmail.com>
2016-11-25 15:19:54 +0000

    Recipe api setup and capturing labels

--- lib/nebula_recipe_bot.rb
+++ lib/nebula_recipe_bot.rb
@@ -5,6 +5,20 @@ require 'dotenv'
 Dotenv.load
 
 token = ENV["TELEGRAM_TOKEN"]
+app_id = ENV["APP_ID"]
+app_key = ENV["APP_KEY"]
+url = "https://api.edamam.com/search?q=chicken&app_id=#{app_id}&app_key=#{app_key}"
+
+recipe = open(url)
+
+labels = []
+label = JSON.parse(recipe.read)
+
+label['hits'].each do |title|
+  labels << title['recipe']['label']
+end
+
+p labels
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
